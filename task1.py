3feae580b132614d08ac0cd5615f6dcf399618e7
JustGoooooo <133954473+JustGoooooo@users.noreply.github.com>
2023-05-18 11:21:21 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,18 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QHBoxLayout
+app = QApplication([])
+G = QWidget()
+G.show()
 
+main_layout = QHBoxLayout()
+
+b1 = QPushButton("   ")
+b2 = QPushButton("   ")
+b3 = QPushButton("   ")
+
+main_layout.addWidget(b1)
+main_layout.addWidget(b2)
+main_layout.addWidget(b3)
+
+
+G.setLayout(main_layout)
+app.exec()
